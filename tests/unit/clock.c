f6a33f9d62d01acde03d8b3b59deaa519f1ccbf4
Ceyhun Şen <ceyhuusen@gmail.com>
2025-10-14 15:51:51 +0300

    tests: Add initial clock tests.

---
+++ tests/unit/clock.c
@@ -0,0 +1,77 @@
+/**
+ * @file
+ * @author Ceyhun Şen
+ * @brief Unit tests for clock module.
+ */
+
+// SPDX-FileCopyrightText: 2025 Ceyhun Şen <ceyhuusen@gmail.com>
+// SPDX-License-Identifier: MIT
+
+#include "hal_clock.h"
+#include "unity.h"
+
+#include <avr/io.h>
+#include <stdint.h>
+#include <test_mock_up.h>
+
+//! \ref hal_clock_oscillator_calibration needs to be 1 byte.
+void test_calibration_struct_size() {
+    OSCCAL = (uint8_t)0b11111111;
+    struct hal_clock_oscillator_calibration val =
+        hal_clock_read_oscillator_calibration();
+
+    TEST_ASSERT_EQUAL(sizeof val, 1);
+}
+
+void test_read_and_parse_osccal() {
+    {
+        OSCCAL = 0;
+        struct hal_clock_oscillator_calibration val =
+            hal_clock_read_oscillator_calibration();
+        TEST_ASSERT_EQUAL(val.range, 0);
+        TEST_ASSERT_EQUAL(val.calibration_value, 0);
+    }
+
+    {
+        OSCCAL = (uint8_t)0b10000000;
+        struct hal_clock_oscillator_calibration val =
+            hal_clock_read_oscillator_calibration();
+        TEST_ASSERT_EQUAL(val.range, 1);
+        TEST_ASSERT_EQUAL(val.calibration_value, 0);
+    }
+
+    {
+        OSCCAL = (uint8_t)0b01111111;
+        struct hal_clock_oscillator_calibration val =
+            hal_clock_read_oscillator_calibration();
+        TEST_ASSERT_EQUAL(val.range, 0);
+        TEST_ASSERT_EQUAL(val.calibration_value, 0b01111111);
+    }
+
+    {
+        OSCCAL = (uint8_t)0b11111111;
+        struct hal_clock_oscillator_calibration val =
+            hal_clock_read_oscillator_calibration();
+        TEST_ASSERT_EQUAL(val.range, 1);
+        TEST_ASSERT_EQUAL(val.calibration_value, 0b01111111);
+    }
+
+    {
+        OSCCAL = (uint8_t)0b11111011;
+        struct hal_clock_oscillator_calibration val =
+            hal_clock_read_oscillator_calibration();
+        TEST_ASSERT_EQUAL(val.range, 1);
+        TEST_ASSERT_EQUAL(val.calibration_value, 0b01111011);
+    }
+}
+
+int main() {
+    RUN_TEST(test_read_and_parse_osccal);
+    RUN_TEST(test_calibration_struct_size);
+
+    return UnityEnd();
+}
+
+void setUp() { reset_registers(); }
+
+void tearDown() { reset_registers(); }
