f8f45c266d5194d523094d30251daf5c88aecbbf
Ceyhun Şen <ceyhuusen@gmail.com>
2025-10-15 12:30:11 +0300

    feat(tests): Introduce spawn_watcher_thread for register watching.

--- tests/mocks/test_mock_up.c
+++ tests/mocks/test_mock_up.c
@@ -4,12 +4,14 @@
  * @brief Mock-up register definitions for testing.
  */
 
-// SPDX-FileCopyrightText: 2023 Ceyhun Şen <ceyhuusen@gmail.com>
+// SPDX-FileCopyrightText: 2025 Ceyhun Şen <ceyhuusen@gmail.com>
 // SPDX-License-Identifier: MIT
 
+#include "test_mock_up.h"
+
+#include <pthread.h>
 #include <stdint.h>
 #include <string.h>
-#include <test_mock_up.h>
 
 /**
  * @brief Virtual mock-up memory for ATmega328P registers.
@@ -22,4 +24,14 @@ uint8_t __atmega328p_registers[0xFF];
 void reset_registers() {
     // Reset everything to zero.
     memset(__atmega328p_registers, 0, sizeof __atmega328p_registers);
+}
+
+/**
+ * @brief Spawns a thread that will watch for changes in registers and does it's
+ * stuff. This is useful especially for interrupts.
+ * @param handler Function that will watch for changes and will do it's stuff.
+ */
+void spawn_watcher_thread(void *(*handler)()) {
+    pthread_t t;
+    pthread_create(&t, NULL, handler, NULL);
 }
