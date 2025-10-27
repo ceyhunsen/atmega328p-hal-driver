a93a4ebcd983affabd46df5fcdcbaad2a8fcee1b
Ceyhun Şen <ceyhuusen@gmail.com>
2025-10-28 00:24:41 +0300

    feat(power): Add power on and off logic to hal_power_change_module_powers.

--- src/hal_power.c
+++ src/hal_power.c
@@ -81,15 +81,29 @@ int hal_power_set_module_power(enum hal_power_modules module, uint8_t state) {
  * @param power_on_list Modules to power on. Set specific bit to 1 to do that.
  * @returns 0 if successful. 1 if there is a problem.
  */
-int hal_power_change_module_powers(uint8_t power_off_list,
-                                   uint8_t power_on_list) {
+enum hal_result_power hal_power_change_module_powers(uint8_t power_off_list,
+                                                     uint8_t power_on_list) {
     // Check if the reserved bit is used.
     if (power_off_list == BIT(4) || power_on_list == BIT(4)) {
-        return 1;
+        return hal_result_power_bit_is_reserved;
     }
 
     // Check if power off and on lists has same bit set.
     if (!(power_off_list ^ power_on_list)) {
-        return 1;
+        return hal_result_power_same_bit_set_for_power_management;
     }
+
+    uint8_t new_state = PRR;
+    new_state |= power_off_list;
+
+    int i;
+    for (i = 0; i <= 8; i++) {
+        if (power_on_list & BIT(i)) {
+            CLEAR_BIT(new_state, i);
+        }
+    }
+
+    PRR = new_state;
+
+    return hal_result_power_ok;
 }
