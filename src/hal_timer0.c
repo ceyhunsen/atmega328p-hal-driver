a36686dfee41c69af2480a5462b5842c28203715
Ceyhun Şen <ceyhuusen@gmail.com>
2026-04-12 21:01:41 +0300

    feat(timer0): Implement hal_timer0_set_output_compare_mode fn.

--- src/hal_timer0.c
+++ src/hal_timer0.c
@@ -9,6 +9,10 @@
 // SPDX-License-Identifier: MIT
 
 #include "hal_timer0.h"
+#include "hal_internals.h"
+
+#include <avr/io.h>
+#include <stdint.h>
 
 enum hal_result_timer0
 hal_timer0_set_output_compare_mode(enum hal_timer0_output_compare_mode mode) {
@@ -16,6 +20,30 @@ hal_timer0_set_output_compare_mode(enum hal_timer0_output_compare_mode mode) {
         mode > hal_timer0_compare_output_mode_set) {
         return hal_result_timer0_invalid_output_compare_mode;
     }
+
+    volatile uint8_t reg_val = TCCR0A;
+
+    switch (mode) {
+    default:
+    case hal_timer0_compare_output_mode_normal:
+        CLEAR_BIT(reg_val, COM0A1);
+        CLEAR_BIT(reg_val, COM0A0);
+        break;
+    case hal_timer0_compare_output_mode_toggle:
+        CLEAR_BIT(reg_val, COM0A1);
+        SET_BIT(reg_val, COM0A0);
+        break;
+    case hal_timer0_compare_output_mode_clear:
+        SET_BIT(reg_val, COM0A1);
+        CLEAR_BIT(reg_val, COM0A0);
+        break;
+    case hal_timer0_compare_output_mode_set:
+        SET_BIT(reg_val, COM0A1);
+        SET_BIT(reg_val, COM0A0);
+        break;
+    }
+
+    TCCR0A = reg_val;
 
     return hal_result_timer0_ok;
 }
--- tests/unit/timer0.c
+++ tests/unit/timer0.c
@@ -16,9 +16,29 @@
 
 #include <avr/io.h>
 
+/// @brief Try to change COM0A* bits and check if operation is successful or
+/// not.
+/// @param mode Mode to be tested
+/// @param expected Expected register value
+/// @param initial_val Initial value of the register
+void set_and_test(enum hal_timer0_output_compare_mode mode, uint8_t expected,
+                  uint8_t initial_val) {
+    printf("For mode %d, expected value is %d\n", mode, expected);
+
+    TEST_ASSERT_EQUAL(hal_timer0_set_output_compare_mode(mode),
+                      hal_result_timer0_ok);
+
+    TEST_ASSERT_EQUAL(TCCR0A >> 6, expected);
+    TEST_ASSERT_EQUAL(TCCR0A & initial_val, initial_val);
+}
 void test_set_output_compare_mode() {
     enum hal_timer0_output_compare_mode mode;
 
+    // Initial value shouldn't be changed, except COM0A* bits.
+    uint8_t initial_val = 0b00001010;
+    TCCR0A = initial_val;
+
+    // Invalid modes should return error.
     mode = hal_timer0_compare_output_mode_set + 1;
     TEST_ASSERT_EQUAL(hal_timer0_set_output_compare_mode(mode),
                       hal_result_timer0_invalid_output_compare_mode);
@@ -26,9 +46,10 @@ void test_set_output_compare_mode() {
     TEST_ASSERT_EQUAL(hal_timer0_set_output_compare_mode(mode),
                       hal_result_timer0_invalid_output_compare_mode);
 
-    mode = hal_timer0_compare_output_mode_normal;
-    TEST_ASSERT_EQUAL(hal_timer0_set_output_compare_mode(mode),
-                      hal_result_timer0_ok);
+    set_and_test(hal_timer0_compare_output_mode_normal, 0b00, initial_val);
+    set_and_test(hal_timer0_compare_output_mode_toggle, 0b01, initial_val);
+    set_and_test(hal_timer0_compare_output_mode_clear, 0b10, initial_val);
+    set_and_test(hal_timer0_compare_output_mode_set, 0b11, initial_val);
 }
 
 int main() {
